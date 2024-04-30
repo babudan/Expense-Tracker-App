deea57113dd7efd159b620ae9b2a06460f09d02d
arindam <babudan517@gmail.com>
2024-04-30 22:24:32 +0530

    update vercel.json

--- my-node-app/index.js
+++ my-node-app/index.js
@@ -10,24 +10,29 @@ app.use(express.json());
 
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.DB,
-    {useNewUrlParser: true}).then(() => console.log('MongoDb is connected')).catch(err => console.log(err))
+    { useNewUrlParser: true }).then(() => console.log('MongoDb is connected')).catch(err => console.log(err))
 
-app.get("/api/test" ,(req,res) => {
-    res.json({body : "test ok1111"})
+
+app.get("/", (req, res) => {
+    res.json({ body: "api is wroking" })
 })
 
-app.post("/api/transaction" ,async(req,res) => {
-    const {name ,datetime ,description ,price} = req.body;
-   const transaction = await Transactionmodel.create({name ,datetime ,description ,price});
+app.get("/api/test", (req, res) => {
+    res.json({ body: "test ok1111" })
+})
+
+app.post("/api/transaction", async (req, res) => {
+    const { name, datetime, description, price } = req.body;
+    const transaction = await Transactionmodel.create({ name, datetime, description, price });
     // console.log(transaction);
     res.json(transaction);
 })
 
-app.get("/api/transactions" ,async(req,res) => {
+app.get("/api/transactions", async (req, res) => {
     const findtransactions = await Transactionmodel.find();
     // console.log(findtransactions);
     res.json(findtransactions);
 })
-app.listen(process.env.PORT , function(){
-    console.log("Express app running on port" + process.env.PORT );
+app.listen(process.env.PORT, function () {
+    console.log("Express app running on port" + process.env.PORT);
 })
