0184c0de2e795b821da17b7077feb7a9361b9288
arindam <babudan517@gmail.com>
2024-05-01 14:31:26 +0530

    update index.js backend

--- my-node-app/index.js
+++ my-node-app/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: "*",
+        origin: "https://expense-tracker-app-backend-hazel.vercel.app",
         methods: ['GET', 'POST'],
         credentials: true
     }
